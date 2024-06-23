b2a36880dc2adc84be52703ff227e5317d979ece
dayvd100 <allysondayv@gmail.com>
2024-06-23 00:23:32 -0300

    Api pronta, falta integrar o selenium

--- sportGeScraping/main.py
+++ sportGeScraping/main.py
@@ -1,11 +1,12 @@
 import requests
 
 # definindo a url da requisicao e os dados que vao ser passados
-url = "http://127.0.0.1:8000/datas/"
+url_datas = "http://127.0.0.1:8000/datas/"
+url_informations = "http://127.0.0.1:8000/informations/"
 data = {"team_name": "Flamengo", "team_point": 50}
 
 # pegando a resposta da requisicao
-response_post_datas_team = requests.post(url, json=data)
+response_post_datas_team = requests.post(url_datas, json=data)
 
 # verificando se os status code da requisicao foi ok
 if response_post_datas_team.status_code == 200:
@@ -14,7 +15,7 @@ else:
     print("Erro")
 
 # verificando se os status code da requisicao foi ok
-response_get_datas_team = requests.get("http://127.0.0.1:8000/informations/")
+response_get_datas_team = requests.get(url_informations)
 
 if response_get_datas_team.status_code == 200:
     print(
